326696be9e74d00daaa1a6eb0e506fd8efb024bf
Gauguin94 <98927470+Gauguin94@users.noreply.github.com>
2022-05-27 21:27:39 +0900

    Add files via upload

--- 02_end_to_end_machine_learning_project/random_forest.py
+++ 02_end_to_end_machine_learning_project/random_forest.py
@@ -4,13 +4,13 @@ import numpy as np
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.model_selection import cross_val_score
 from sklearn.metrics import mean_squared_error
+from sklearn.model_selection import GridSearchCV
 import joblib
 
 def display_scores(scores):
     print("score:", scores)
     print("mean:", scores.mean())
     print("standard deviation:", scores.std())
-
 
 if __name__ == "__main__":
     dataloader = dataLoader()
@@ -30,4 +30,17 @@ if __name__ == "__main__":
     scores = cross_val_score(rfr, x, label, scoring="neg_mean_squared_error", cv=10)
     forest_rmse_scores = np.sqrt(-scores)
     display_scores(forest_rmse_scores)
+
+    param_grid = [
+        {'n_estimators': [3, 10, 30], 'max_features': [2, 4, 6, 8]},
+        {'bootstrap': [False], 'n_estimators': [3, 10], 'max_features': [2, 3, 4]}
+    ]
+    forest_reg = RandomForestRegressor()
+    grid_search = GridSearchCV(forest_reg, param_grid, cv=5,
+                            scoring='neg_mean_squared_error',
+                            return_train_score=True)
+    grid_search.fit(x, label)
+    grid_search.best_params_    
+
+
     joblib.dump(rfr, "forest_model.pkl")
